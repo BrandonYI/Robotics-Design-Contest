ef1c03658761b95cae10178201ea090e6287bf2b
forsythe <fjorsyth@gmail.com>
2016-11-03 20:07:08 +0800

    merged stuff

--- src/user/main.c
+++ src/user/main.c
@@ -1,78 +1,113 @@
+
 #include "main.h"
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 u32 ticks_img = 0;
 u32 ticks_sec_img = 0;
 u16 servo_pos = 750;
 u8 speed = 20;
 u16 speed_indic[3] = {RGB888TO565(0xC72929), RGB888TO565(0xFFC72C), RGB888TO565(0x40CA77)};
+const int WINDOWSIZE = 30;
+const int MAXBUFFER = 100;
 
 int global_led_on = 0;
-
-const int MAXBUFFER = 100;
-char buffer[MAXBUFFER] = {0};
-int pointer = 0;
-
-char cmd[10] = "";
-char val[10] = "";
-
 int bool_need_clear_buffer = 1;
 int bool_command_finish = 0;
-
-char curKey = '\0';
-
-void uart_listener_buffer(const u8 byte) {
-    buffer[pointer++] = byte;
-    uart_tx(COM3, "BUFFER: %s\n", buffer);
-
-    if (byte == '.') {
-        bool_command_finish = 1;
-        pointer = 0;
-    }
-}
-
 int timeSinceLastCommand;
 int curTime;
+int ccdTime = 0;
+int ccd_rate = 50;
+int buffer_index = 0;
 
+u8 medianCCD[128] = {0};
+u8 schmittCCD[128] = {0};
+
+char buffer[MAXBUFFER] = {0};
+char manual_buffer[100];
+char curKey = '\0';
+
+void use_motor(long value, long id) {
+    if(value<100 && value>0) {
+        motor_control((MOTOR_ID)id, 1, value);
+    } else {
+        uart_tx(COM3, "motor value is out of range\n");
+    }
+}
+void use_servo(long value, long id) {
+    if(value<1050&&value>450) {
+        servo_control((SERVO_ID)id, value);
+    } else {
+        uart_tx(COM3, "servo value is out of range\n");
+    }
+}
+void use_pneumatic(long value, long id) {
+    pneumatic_control((PNEUMATIC_ID)id, value); //1 is on, others is off
+}
+void use_led(long value, long id) {
+    if(value == 1) {
+        led_on((LED_ID)id);
+        uart_tx(COM3, "TURNED LED %ld ON\n", id);
+    } else {
+        led_off((LED_ID)id);
+        uart_tx(COM3, "TURNED LED %ld OFF\n", id);
+    }
+}
+void buffer_clear() {
+    if (bool_need_clear_buffer) {
+        bool_need_clear_buffer = 0;
+        buffer_index = 0;
+        uart_tx(COM3, "\nBuffer: ");
+    }
+}
+void uart_listener_buffer(const u8 byte) {
+
+}
 void uart_listener(const u8 byte) {
     curTime = get_real_ticks();
     timeSinceLastCommand = 0;
-
-    if (curKey != byte)
-        uart_tx(COM3, "recieved: %c\n", byte);
-
-    curKey = byte;
-}
-
-int strequals(const char a[], int aLength, const char b[]) {
-    int k;
-    for (k = 0; k < aLength; k++) {
-        if (a[k] != b[k])
-            return 0;
+    buffer[buffer_index++] = byte;
+    buffer[buffer_index] = '\0';
+    uart_tx(COM3, "%c", byte);
+    if (byte == '.') {
+        bool_command_finish = 1;
+        buffer_index = 0;
     }
-    return 1;
+    if(byte == 'x') {	//If you make a typo, press x to reset buffer
+        bool_need_clear_buffer = 1;
+        buffer_index = 0;
+    }
+    /*if (curKey != byte){
+        uart_tx(COM3, "received: %c\n", byte);
+    }
+    curKey = byte;*/
 }
-
-const int WINDOWSIZE = 20; //for median
-int medianCCD[128] = {0};
-int schmittCCD[128] = {0};
-
 float getMedian(const int a[]) {
     int arr[WINDOWSIZE] = {0};
     for (int k = 0; k < WINDOWSIZE; k++) { //copy into temporary array
         arr[k] = a[k];
     }
-    int key, j;
-    for (int i = 2; i < WINDOWSIZE; i++) { //selection sort
+    int key, i, j;
+    for (i = 2; i < WINDOWSIZE; i++) { //selection sort
         key = arr[i];
         j = i - 1;
         while (j>0 && arr[j] > key) {
             arr[j+1] = arr[j];
-            j--;
+            --j;
         }
         arr[j+1] = key;
     }
     return (WINDOWSIZE%2==1? arr[WINDOWSIZE/2] : (arr[WINDOWSIZE/2-1]+arr[WINDOWSIZE/2])/2);
 }
+const int WHITE_THRESHOLD = 130;
+void runSchmitt() {
+    int k;
+    for (k = 0; k < 128; k++) {
+        schmittCCD[k] = (medianCCD[k] < WHITE_THRESHOLD)? 1: 158;
+    }
+}
+
 
 void runMedianFilter() {
     int curWindow[WINDOWSIZE] = {0};
@@ -81,7 +116,6 @@ void runMedianFilter() {
     for (int k = 0; k < WINDOWSIZE; k++) {
         curWindow[k] = linear_ccd_buffer1[k];
     }
-
     //int firstMaxMedianIndex = 0;
     //int lastMaxMedianIndex = 0;
     for (int j = 0; j <= WINDOWSIZE/2; j++) {
@@ -101,126 +135,125 @@ void runMedianFilter() {
         }
         */
     }
-
     for (int k = 128 - WINDOWSIZE; k < 128; k++) {
         medianCCD[k] = getMedian(curWindow);
     }
 }
+void bluetooth_handler() {
+    if (bool_command_finish) {
+        bool_command_finish = 0;
+        uart_tx(COM3, "\nCOMPLETE COMMAND: %s\n", buffer);
 
-const int WHITE_THRESHOLD = 130;
-void runSchmitt() {
-    int k;
-    for (k = 0; k < 128; k++) {
-        schmittCCD[k] = (medianCCD[k] < WHITE_THRESHOLD)? 1: 158;
+        char* cmdptr = strchr(buffer, ':');	//Locate ptr where the char : is first found
+        char* valptr = cmdptr + 1;
+        char* idptr = cmdptr - 1;
+        int val = strtol(valptr, NULL,10); //Obtain Value
+        *cmdptr = '\0';
+        int id = strtol(idptr, NULL,10); //Obtain ID
+        *idptr = '\0';
+        uart_tx(COM3, "COMMAND: %s   ", buffer);
+        uart_tx(COM3, "ID: %ld   ", id);
+        uart_tx(COM3, "VAL: %ld\n", val);
+
+        if(strstr(buffer, "led")) { //if detect substring led(strstr returns a pointer)
+            bool_need_clear_buffer = 1;
+            use_led(val, id); //LED
+        } else if(strstr(buffer,"motor")) { //if detect substring motor
+            use_motor(val, id); //MOTOR
+            uart_tx(COM3,"motor %ld is on \n", id);
+        } else if(strstr(buffer,"servo")) { //if detect substring servo
+            use_servo(val, id); //SERVO
+            uart_tx(COM3, "servo %ld is on \n", id);
+        } else if(strstr(buffer,"pneumatic")) {
+            use_pneumatic(val, id); //PNEUMATIC
+            uart_tx(COM3, "pneumatic %ld is on \n", id);
+        }
+        bool_need_clear_buffer = 1;
     }
+    /*//TODO: Consider adding a manual command so that letters do not overlap? Test if current program will work on STM32
+    int i;
+    for (i = 0; buffer[i] != '\0'; ++i) //to account for the rare simulateneous inputs
+    {
+    	if (buffer[i] == 'w'){ //considering switch statements for readibility
+    		uart_tx(COM3, "w "); //up arrow 0x0E
+    	}
+    	else if (buffer[i] == 'a'){//How to account for the a in 'pneumatic'
+    		uart_tx(COM3, "a "); //left arrow 0x0B
+    	}
+    	else if (buffer[i] == 's'){ //How to account for the s when typing in the command 'servo'
+    		uart_tx(COM3, "s "); //down arrow 0x0C
+    	}
+    	else if (buffer[i] == 'd'){
+    		uart_tx(COM3, "d "); //right arrow 0x07
+    	}
+    }
+    if(buffer[i] == '\0' ){ //if the first element of buffer is \0 or the we reached the end of the wasd loop
+    	bool_need_clear_buffer = 1; //TODO:Set a time interval where the command is not nullified
+    }*/
+    buffer_clear();
 }
-
-int main() {
+void init_all() {
     led_init();
     gpio_init();
     ticks_init();
     linear_ccd_init();
     adc_init();
     button_init();
-
-    tft_init(2, BLACK, WHITE);
+    servo_init(143,10000,0);
+    tft_init(1, BLACK, WHITE);
     uart_init(COM3, 115200);
     uart_interrupt_init(COM3, &uart_listener); //com port, function
     uart_tx(COM3, "initialize\n");
+    motor_init(143, 10000, 0);
+}
+/*	if(strcmp("manual",cmd)==0){
+							if(curKey=='w'){
 
-    int ccdTime = 0;
 
-    int h;
-
-
-    while (1) {
-
-        if (curKey != '\0' && get_real_ticks() - curTime >= 200) {
+							}
+						if (curKey != '\0' && get_real_ticks() - curTime >= 200) {
             uart_tx(COM3, "%c released\n", curKey);
             curKey = '\0';
             curTime = get_real_ticks();
         }
+			}
 
-        if (get_real_ticks() - ccdTime >= 50) {
+
+						}
+						if(strcmp("auto",cmd)==0){
+
+
+				}*/
+int main() {
+    init_all();
+    while(1) {
+        motor_control(0, 1, 5); //id, direction, magnitude
+        if (get_real_ticks() - ccdTime >= ccd_rate) { //Update by CCD Rate
             ccdTime = get_real_ticks();
-
             int k;
-            for (k = 0; k < 128; k++) {
+            for (k = 0; k < 128; k++) { //Clear CCD Screen
                 tft_put_pixel(k, 159-linear_ccd_buffer1[k], BLACK);
                 tft_put_pixel(k, 159-medianCCD[k], BLACK);
                 tft_put_pixel(k, 159-schmittCCD[k], BLACK);
+
             }
             linear_ccd_read();
             runMedianFilter();
             runSchmitt();
-            for (k = 0; k < 128; k++) {
-                //uart_tx(COM3, "%d ", linear_ccd_buffer1[k]);
+
+            for (k = 0; k < 128; k++) { //Add CCD onto Screen
                 tft_put_pixel(k, 159-linear_ccd_buffer1[k], RED);
                 tft_put_pixel(k, 159-schmittCCD[k], GREEN);
+
                 tft_put_pixel(k, 159-medianCCD[k], WHITE);
             }
-            //uart_tx(COM3, "\n");
-
             for(h = 0; h < 159; h+=20) {
                 tft_prints(2, h, "%d", 159-h);
             }
         }
-
-        if (bool_command_finish) {
-            bool_command_finish = 0;
-            uart_tx(COM3, "COMPLETE COMMAND: %s\n", buffer);
-
-            int k;
-            for (k = 0; buffer[k] != ':'; k++) {
-                cmd[k] = buffer[k];
-            }
-            int j;
-            int valPoint = 0;
-            for (j = k+1; buffer[j] != '.'; j++) {
-                val[valPoint++] = buffer[j];
-            }
-            uart_tx(COM3, "NAME: %s\n", cmd);
-            uart_tx(COM3, "VAL: %s\n", val);
-
-            if (strequals("led", 3, cmd)) {
-                if(strequals("1", 1, val)) {
-                    led_on(LED1);
-                    uart_tx(COM3, "TURNED LED 1 ON\n");
-                } else {
-                    led_off(LED1);
-                    uart_tx(COM3, "TURNED LED 1 OFF\n");
-                }
-            }
-
-            if (strcmp("led", cmd) == 0) {
-                if (strcmp("1", val) == 0) {
-                    led_on(LED1);
-                    uart_tx(COM3, "TURNED LED 1 ON\n");
-                } else {
-                    led_off(LED1);
-                    uart_tx(COM3, "TURNED LED 1 OFF\n");
-                }
-            }
-
-            //insert more command checking here
-
-            bool_need_clear_buffer = 1;
-        }
-
-        if (bool_need_clear_buffer) {
-            bool_need_clear_buffer = 0;
-            int k;
-            for (k = 0; k < MAXBUFFER; k++) {
-                buffer[k] = '\0';
-            }
-            for (k = 0; k < 10; k++) {
-                cmd[k] = '\0';
-                val[k] = '\0';
-            }
-        }
+        bluetooth_handler();
     }
 }
-
 
 //Button listeners
 /*
@@ -247,99 +280,4 @@ void uart3_listener(const u8 byte) {
     u8 ledno = byte - (u8)'1';
     if (ledno >= 3) return;
     led_toggle((LED_ID)ledno);
-}
-
-
-int main2() {
-    ticks_init();
-    led_init();
-    gpio_init();
-    pneumatic_init();
-
-    button_init();
-    set_keydown_listener(BUTTON2, &change_speed);
-
-    tft_init(2, BLACK, WHITE);
-    uart_init(COM3, 115200);
-    uart_interrupt_init(COM3, &uart_listener); //com port, function
-    uart_tx(COM3, "initialize\n");
-
-    tft_prints(0, 0, "tft start");
-
-    tft_init(3, BLACK, RGB888TO565(0xFF0080));
-    tft_prints(0, 0, "Hello World!");
-    tft_set_text_color(WHITE);
-    tft_prints(0, 12, "My name is Pang.");
-    tft_prints(0, 24, "I am a single & toxic guy :)");
-    tft_prints(0, 60, "Seconds:");
-    tft_prints(46, 60, "0");
-    tft_prints(0, 72, "Servo:");
-    tft_prints(46, 72, "750");
-    //tft_set_font(&serifGothic_16ptFontInfo);
-    //tft_prints(0, 92, "War Dragon");
-    //tft_set_font(&microsoftSansSerif_8ptFontInfo);
-    //tft_print_image(0, 120, Logo);
-
-    tft_fill_area(72, 72, 12, 12, RED);
-
-    tft_fill_area(10, 96, 57, 108, WHITE);
-    tft_fill_area(12, 98, 55, 106, BLACK);
-
-    uart_init(COM3, 115200);
-    uart_tx(COM3, "Test\n");
-    uart_interrupt_init(COM3, &uart3_listener);
-
-
-    servo_init(143, 10000, 0);
-    adc_init();
-    linear_ccd_init();
-
-
-    while (1) {
-
-        if (ticks_img != get_real_ticks()) {
-            ticks_img = get_real_ticks();
-            if (ticks_img % 50 == 0) {
-                button_update();
-
-                /*
-                if (read_button(BUTTON1) == 0 && servo_pos < 1050) {
-                	servo_pos += speed;
-                	tft_fill_area(46, 72, 25, 12, BLACK);
-                	tft_prints(46, 72, "%d", servo_pos);
-                }
-
-                if (read_button(BUTTON3) == 0 && servo_pos > 450) {
-                	servo_pos -= speed;
-                	tft_fill_area(46, 72, 25, 12, BLACK);
-                	tft_prints(46, 72, "%d", servo_pos);
-                }
-                */
-                //}
-
-                if (ticks_img % 1000 == 0) {
-                    tft_fill_area(46, 60, 25, 12, BLACK);
-                    tft_prints(46, 60, "%d", get_second_ticks());
-                }
-
-                if (ticks_img % 10 == 0) {
-                    for (u8 i=0; i<128; i++)
-                        tft_put_pixel(i, 159 - linear_ccd_buffer1[i], BLACK);
-                    linear_ccd_read();
-                    for (u8 i=0; i<128; i++)
-                        tft_put_pixel(i, 159 - linear_ccd_buffer1[i], WHITE);
-                }
-            }
-
-            /*servo_control(SERVO1, servo_pos);
-            if (read_gpio(GPIOA, GPIO_Pin_11)) {
-            	led_on(LED1);
-            	pneumatic_control(GPIOC, GPIO_Pin_0, 1);
-            } else {
-            	led_off(LED1);
-            	pneumatic_control(GPIOC, GPIO_Pin_0, 0);
-            }*/
-
-        }
-    }
 }
