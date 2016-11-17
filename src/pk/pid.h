e7e4803ae2eb10b19a66b70a15d3180ef36a290f
Christopher Wong <pkcwong@users.noreply.github.com>
2016-11-18 00:20:38 +0800

    Create pid.h

---
+++ src/pk/pid.h
@@ -0,0 +1,30 @@
+#ifndef _PK_PID
+#define _PK_PID
+
+#include "ticks.h"
+
+struct Reading
+{	
+	int value;
+	int ticks;
+};
+typedef struct Reading Reading;
+
+struct PID
+{
+	double kp;
+	double ki;
+	double kd;
+	int proportion;
+	int integral;
+	int derivative;
+	Reading current;
+	Reading prev;
+};
+typedef struct PID PID;
+
+void pid_init(PID* tracker, double p, double i, double d);
+void pid_sampling(PID* tracker, int reading);
+int pid_output(PID* tracker, int target);
+
+#endif
