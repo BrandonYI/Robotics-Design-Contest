e3e9d97eb057b4e9b9123b58a45979ee8e795ac1
Christopher Wong <pkcwong@users.noreply.github.com>
2016-11-18 21:44:51 +0800

    Create encoder.h

---
+++ src/pk/encoder.h
@@ -0,0 +1,29 @@
+#ifndef _ENCODER
+#define _ENCODER
+
+#include "ticks.h"
+
+typedef enum {
+	ENCODER_LEFT,
+	ENCODER_RIGHT
+}ENCODER_ID;
+
+struct Encoder
+{
+	int id;
+	int rotations;
+	int prev;
+	int prev_tick;
+	int current;
+	int current_tick;
+	int prev_real;
+	int current_real;
+};
+typedef struct Encoder Encoder;
+
+void encoder_init(Encoder* encoder, ENCODER_ID id);
+void encoder_update(Encoder* encoder);
+int get_encoder_value(Encoder* encoder);
+int get_encoder_velocity(Encoder* encoder);
+
+#endif
